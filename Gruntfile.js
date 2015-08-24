0d201988c2ea7929fc7b74166524b41fa5a4cc5a
kamodev <webdev@kamobagula.com>
2015-08-24 07:23:05 -0700

    Added more files to the repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,22 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
+    },
+    sass: {
+      styles: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        src: 'styles.scss',
+        dest: 'styles.css'
+      }
     }
   });
 
   // Load the tasks
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Register tasks
   grunt.registerTask('default', ['jshint']);
